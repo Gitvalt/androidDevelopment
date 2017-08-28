cc64be6a76700b6c6fcd8ef917cab96a7909a16d
Valtteri Seuranen <K1967@Labranet.jamk.fi>
2017-08-28 10:44:42 +0300

    harjoitusta 2 päivitetään

--- BasicUIControls/app/src/main/java/com/example/k1967/basicuicontrols/MainActivity.java
+++ BasicUIControls/app/src/main/java/com/example/k1967/basicuicontrols/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.k1967.basicuicontrols;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selectButtonClicked(View view){
+        RadioGroup rg = (RadioGroup) findViewById(R.id.myNiceRadioGroup);
+        int id = rg.getCheckedRadioButtonId();
+        RadioButton button = (RadioButton)findViewById(id);
+        String text = (String)button.getText();
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
     }
 }
