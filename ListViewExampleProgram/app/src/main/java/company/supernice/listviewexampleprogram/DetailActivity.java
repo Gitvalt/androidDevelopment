208bb244f81ee99aa6ca5a1ef97b260f539e80c2
gitvalt <valtteri.seuranen@gmail.com>
2017-09-02 21:15:47 +0300

    video test

--- ListViewExampleProgram/app/src/main/java/company/supernice/listviewexampleprogram/DetailActivity.java
+++ ListViewExampleProgram/app/src/main/java/company/supernice/listviewexampleprogram/DetailActivity.java
@@ -1,7 +1,11 @@
 package company.supernice.listviewexampleprogram;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by Valtteri on 30.8.2017.
@@ -12,7 +16,36 @@ public class DetailActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_layout);
+        setContentView(R.layout.new_layout);
+
+        //get the Intent that has openned this activity
+        Intent intent = getIntent();
+
+        //get the data from the intent
+        Bundle bundle = intent.getExtras();
+
+        //get the phone name from intent
+        String phone = bundle.getString("phone");
+
+        //setup the phone name
+        TextView textView = (TextView) findViewById(R.id.phoneTextView);
+        textView.setText(phone);
+
+        //show the corrent phone logo
+        ImageView imageView = (ImageView)findViewById(R.id.phoneImageView);
+
+        switch (phone) {
+            case "Android": imageView.setImageResource(R.drawable.android); break;
+            case "Iphone": imageView.setImageResource(R.drawable.ios); break;
+            case "WindowsMobile": imageView.setImageResource(R.drawable.windows); break;
+            case "Blackberry": imageView.setImageResource(R.drawable.blackberry); break;
+            case "WebOS": imageView.setImageResource(R.drawable.webos); break;
+            case "Ubuntu": imageView.setImageResource(R.drawable.ubuntu); break;
+        }
+    }
+
+    public void backButtonPressed(View view){
+        finish();
     }
 
 }
--- ListViewExampleProgram/app/src/main/java/company/supernice/listviewexampleprogram/MainActivity.java
+++ ListViewExampleProgram/app/src/main/java/company/supernice/listviewexampleprogram/MainActivity.java
@@ -1,7 +1,10 @@
 package company.supernice.listviewexampleprogram;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -16,31 +19,46 @@ public class MainActivity extends AppCompatActivity {
 
         //--populate the listview--
 
-        //get the listview element
-        ListView listview = (ListView) findViewById(R.id.mainListView);
+            //get the listview element
+            ListView listview = (ListView) findViewById(R.id.mainListView);
 
-        //create dummy data
-        String[] phones = new String[]{
-                "Android", "Iphone", "WindowsMobile", "Blackberry", "WebOS", "Ubuntu",
-                "Android", "Iphone", "WindowsMobile", "Blackberry", "WebOS", "Ubuntu"
-        };
+            //create dummy data
+            String[] phones = new String[]{
+                    "Android", "Iphone", "WindowsMobile", "Blackberry", "WebOS", "Ubuntu",
+                    "Android", "Iphone", "WindowsMobile", "Blackberry", "WebOS", "Ubuntu"
+            };
 
-        final ArrayList<String> list = new ArrayList<String>();
+            final ArrayList<String> list = new ArrayList<String>();
 
-        //add items from phones array to the arrayList
-        for(int i = 0; i < phones.length; ++i){
-            list.add(phones[i]);
-        }
+            //add items from phones array to the arrayList
+            for(int i = 0; i < phones.length; ++i){
+                list.add(phones[i]);
+            }
 
-        //Default -- ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
-        //test1 -- ArrayAdapter adapter = new ArrayAdapter(this, R.layout.rowlayout, R.id.textView2, list);
+            //Default -- ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+            //test1 -- ArrayAdapter adapter = new ArrayAdapter(this, R.layout.rowlayout, R.id.textView2, list);
 
-        //custom adapter
-        PhoneArrayAdapter adapter = new PhoneArrayAdapter(this, list);
+            //custom adapter
+            PhoneArrayAdapter adapter = new PhoneArrayAdapter(this, list);
 
-        //set adapter to listview
-        listview.setAdapter(adapter);
+            //set adapter to listview
+            listview.setAdapter(adapter);
         //--end populating
+
+        // item listener
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // get list row data (now String as a phone name)
+                String phone = list.get(position);
+                // create an explicit intent
+                Intent intent = new Intent(MainActivity.this,DetailActivity.class);
+                // add data to intent
+                intent.putExtra("phone", phone);
+                // start a new activity
+                startActivity(intent);
+            }
+        });
 
     }
 
