58f4ea9bb7e769ee4f48a4d033bfe2a8fc9c7c93
gitvalt <valtteri.seuranen@gmail.com>
2017-09-27 01:21:19 +0300

    melkein

--- LoadJSON/app/src/main/java/com/development/k1967/loadjson/MainActivity.java
+++ LoadJSON/app/src/main/java/com/development/k1967/loadjson/MainActivity.java
@@ -23,13 +23,17 @@ import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Response;
+import com.google.android.gms.location.FusedLocationProviderApi;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -47,10 +51,12 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     //info textview element
     public TextView infoText;
+
+    private int permissionTag = 123;
 
     //locations defined in the loaded json
     private JSONArray locationArray;
@@ -58,11 +64,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     //map-element
     private GoogleMap map;
 
-
     private FusedLocationProviderClient providerClient;
-    private Activity source;
 
+    //where the phone currently is
+    private Location HomeLocation;
 
+    private String APIKEY = "AIzaSyDZ0GaJniVAIsgNwQetR1f9RHUDpmtofo0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,6 +84,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
 
 
+        providerClient = LocationServices.getFusedLocationProviderClient(this);
+
+        providerClient.requestLocationUpdates(new LocationRequest())
+
+
         //create custom json-fetcher object and define what to do when data has been fetched
         JSONFetcher task = new JSONFetcher() {
             @Override
@@ -89,11 +101,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 try {
                     infoText.setText("Loading json has been completed!");
 
-                    Log.d("Locator", "Locator start!");
+                    locationArray = object.getJSONArray("Locations");
+                    onDataLoaded();
+
+
+                    //Log.d("Locator", "Locator start!");
                     SelfLocator();
 
-                    locationArray = object.getJSONArray("fetchedArray");
-                    onDataLoaded();
+
                 } catch (JSONException ex) {
 
                 }
@@ -104,11 +119,89 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         task.execute("http://student.labranet.jamk.fi/~K1967/androidCourses/dummyJSON.json");
     }
 
+
+        @Override
+        public void onConnected(@Nullable Bundle bundle) {
+
+        }
+
+        @Override
+        public void onConnectionSuspended(int i) {
+
+        }
+
+        @Override
+        public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+
+    private void renderDirections(LatLng destination){
+
+        LatLng homeCoordinates = new LatLng(HomeLocation.getLatitude(), HomeLocation.getLongitude());
+        LatLng destCoordinates = destination;
+
+        try
+        {
+            //Url to directions API
+            URL destinationURL =
+                    new URL("https://maps.googleapis.com/maps/api/directions/json?origin=" + HomeLocation.getLatitude() + "," + HomeLocation.getLongitude() +
+                            "&destination=" + destCoordinates.latitude + "," + destCoordinates.longitude + "&key=" + APIKEY);
+
+
+            JSONFetcher task = new JSONFetcher() {
+                @Override
+                protected void onPreExecute() {
+                    Log.i("Destination_info", "Fetching Destination starts");
+                }
+
+                @Override
+                protected void onPostExecute(JSONObject object) {
+
+                    JSONObject response = object;
+
+                    try
+                    {
+                        //Directions could not be generated for the two markers
+                        if (response.get("status") == "ZERO_RESULTS")
+                        {
+                            Log.e("Destination_ERROR", "Could not generate directions between two valid destinations");
+                        }
+
+                        //origin or source is not valid
+                        else if(response.getString("status") == "NOT_FOUND")
+                        {
+                            Log.e("Destination_ERROR", "Destination or Starting place is not a valid address/coordinate");
+                        }
+
+                        //fetching directions was successfull
+                        else
+                        {
+
+                        }
+
+                    }
+                    catch (JSONException e)
+                    {
+                        Log.e("Destination_ERROR", "Fetching JSON directions has failed");
+                    }
+                }
+            };
+
+            task.execute(destinationURL.toString());
+        }
+        catch (MalformedURLException e)
+        {
+
+        }
+
+    }
+
     //when map creation is complited --> generate listerners
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-        map.setMaxZoomPreference(6.0f);
+        map.setMaxZoomPreference(10.0f);
 
         map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
@@ -116,17 +209,20 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
                 String msg = "Marker: " + marker.getTitle() + " has been pressed!";
                 Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                renderDirections(marker.getPosition());
                 return false;
             }
         });
     }
 
-    //when loading is complited --> create and add markers to map
+    //when loading is completed --> create and add markers to map
     private void onDataLoaded() {
 
         //Has information been loaded?
         if (locationArray.length() > 0) {
-            try {
+            try
+            {
+                //read locations and create markers
                 for (int i = 0; i < locationArray.length(); i++) {
 
                     JSONObject item = locationArray.getJSONObject(i);
@@ -150,75 +246,106 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     }
 
-    public void SelfLocator() {
+    //check permissions for fine_location and continue
+    private void SelfLocator() {
 
-        source = this;
+        int checkPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
 
-        //get location provider
-        providerClient = LocationServices.getFusedLocationProviderClient(source);
-
-        int checkPermission = ContextCompat.checkSelfPermission(source, Manifest.permission.ACCESS_COARSE_LOCATION);
-
+        //if permission for fine_location is not granted
         if(checkPermission != PackageManager.PERMISSION_GRANTED)
         {
-            //ask permission
-            android.support.v4.app.ActivityCompat.requestPermissions(source, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 666);
+            //ask for permission
+            android.support.v4.app.ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, permissionTag);
+
+            //continues in onRequestPermissionResult method when user has granted permission
+
         }
-        else {
+        //if permission has been granted
+        else
+        {
             LoadLocation();
         }
     }
 
+    //when request permission is handled
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode == 666){
+        if(requestCode == permissionTag){
             if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                 LoadLocation();
             }
             else {
-                Log.w("TAGGER", "Permission is denied");
+                Log.e("PERMISSION_FAILURE", "Permission is denied");
             }
         }
     }
 
-    private void gotLocation(Location result){
-        Location item = result;
-    }
-
-    private void LoadLocation(){
-        int checkPermission = ContextCompat.checkSelfPermission(source, Manifest.permission.ACCESS_COARSE_LOCATION);
-
-        if(checkPermission != PackageManager.PERMISSION_GRANTED)
+    //when gps returns phone location
+    private void receiveSelfLocation(Location resultLocation){
+        if(resultLocation == null)
         {
-            //ask permission
-            android.support.v4.app.ActivityCompat.requestPermissions(source, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 666);
-
+            Log.e("Location_ERROR", "Self location was null");
         }
         else
         {
-            providerClient.getLastLocation()
-                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            if(location != null){
 
-                                double i = location.getLatitude();
-                                double y = location.getLongitude();
-                                Log.w("TAGGER", "Location test: " + i + " " + y);
-                                gotLocation(location);
+            HomeLocation = resultLocation;
+
+            map.addMarker(new MarkerOptions()
+                    .position(new LatLng(resultLocation.getLatitude(), resultLocation.getLongitude()))
+                    .title("Self Location")
+                    .snippet("Lat: " + resultLocation.getLatitude() + " Long: " + resultLocation.getLongitude())
+                    .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher))
+            );
+
+        }
+    }
+
+    //read location data from gps
+    private void LoadLocation(){
+        int checkPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        //if no permission for fine_location
+        if(checkPermission != PackageManager.PERMISSION_GRANTED)
+        {
+            //ask for permission
+            android.support.v4.app.ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, permissionTag);
+
+        }
+
+        //if permission is granted for fine_location
+        else
+        {
+
+            //get last known location
+            providerClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>()
+                    {
+                        //when reading last location is successfull
+                        @Override
+                        public void onSuccess(Location location)
+                        {
+                            if(location != null)
+                            {
+                                Log.i("Location_INFO", "Location test: " + location.getLatitude() + " ; " + location.getLongitude());
+                                //send fetched location information to method
+                                receiveSelfLocation(location);
                             }
                             else
                             {
-                                Log.w("TAGGER", "No location read");
+                                Log.e("Location_ERROR", "Location was empty?");
                             }
                         }
                     })
 
-                    .addOnFailureListener(this, new OnFailureListener() {
+                    //when reading last location has failed
+                    .addOnFailureListener(this, new OnFailureListener()
+                    {
                         @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.w("TAGGER_ER", "Reading location failed");
-                            Log.w("TAGGER_ER", e.getMessage());
+                        public void onFailure(@NonNull Exception e)
+                        {
+                            Log.e("Location_ERROR", "Reading location has failed");
+                            Log.e("Location_ERROR", e.getMessage());
                         }
                     });
 
@@ -227,6 +354,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
 
-} //end of activity class
+
+} //end of MainActivity class
 
 
